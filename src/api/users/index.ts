7007169a4975453002a1ac164189fe189bf6e4a6
Felix Orinda <forinda82@gmail.com>
2025-04-05 11:42:42 +0300

    Refactor UsersControllerModule to use GetUsersRepositoryToken for improved dependency injection

--- src/api/users/index.ts
+++ src/api/users/index.ts
@@ -8,7 +8,7 @@ import { CreateUserRepositoryImpl } from './repositories/create-user.repository.
 import { GetUsersRepositoryImpl } from './repositories/get-users.repository.impl';
 import { CreateUserService } from './services/create-user.service';
 import { CreateUserRepositoryToken } from './services/create-user.service';
-import { GetUsersService } from './services/get-users.service';
+import { GetUsersRepositoryToken, GetUsersService } from './services/get-users.service';
 
 @Singleton()
 export class UsersControllerModule extends ControllerModule {
@@ -25,7 +25,7 @@ export class UsersControllerModule extends ControllerModule {
 
     // Bind repositories
     di.bind<CreateUserRepositoryImpl>(CreateUserRepositoryToken).to(CreateUserRepositoryImpl);
-    di.bind<GetUsersRepositoryImpl>(Symbol.for('GetUsersRepository')).to(GetUsersRepositoryImpl);
+    di.bind<GetUsersRepositoryImpl>(GetUsersRepositoryToken).to(GetUsersRepositoryImpl);
 
     // Bind services
     di.bind<CreateUserService>(Symbol.for('CreateUserService')).to(CreateUserService);
