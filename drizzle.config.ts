ef11feeee313d28f8d94a6a35dd5806ed3434232
Felix Orinda <forinda82@gmail.com>
2025-04-05 11:56:16 +0300

    Update drizzle configuration and enhance database commands in package.json

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from 'drizzle-kit';
 import * as dotenv from 'dotenv';
+
 dotenv.config();
 
 export default defineConfig({
-  schema: './src/core/db/schema/index.ts',
-  out: './drizzle',
+  schema: './src/core/db/schema',
+  out: './migrations',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    database: process.env.DB_NAME!,
+    host: process.env.DB_HOST!,
+    port: Number(process.env.DB_PORT ?? '5432'),
+    user: process.env.DB_USER!,
+    password: process.env.DB_PASSWORD!,
+    // ssl: process.env.DB_SSL === 'false',
   },
 });
