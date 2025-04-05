6217b41422cce72933bbc8ed735c426d4a390772
Felix Orinda <forinda82@gmail.com>
2025-04-05 13:00:15 +0300

    Refactor ApiModule to use ControllerModule instead of BaseControllerModule for improved clarity

--- src/core/decorators/api-module.ts
+++ src/core/decorators/api-module.ts
@@ -1,16 +1,16 @@
 import { LoggerService } from '@/common/logger';
-import { BaseControllerModule } from './controller-module';
+import { ControllerModule } from './controller-module';
 
 export const API_MODULE_KEY = Symbol('api-module');
 
 export interface ApiModuleMetadata {
-  modules: (new () => BaseControllerModule)[];
+  modules: (new () => ControllerModule)[];
 }
 
 export function ApiModule(metadata: ApiModuleMetadata) {
   return function (target: any) {
     const logger = new LoggerService();
-    logger.debug(`[ApiModule] Registering API module ${target.name}`);
+    logger.debug('[ApiModule]', `Registering API module ${target.name}`);
 
     // Store module metadata
     Reflect.defineMetadata(API_MODULE_KEY, metadata, target);
